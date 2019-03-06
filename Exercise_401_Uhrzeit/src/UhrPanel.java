4630c6e6d5e57e82a7afb8bed9cbfc8bb3169985
Unknown <zwepab16@htlkaindorf.at>
2019-03-06 12:07:03 +0100

    1. Uhrzeit wird geupdatet!

--- Exercise_401_Uhrzeit/src/UhrPanel.java
+++ Exercise_401_Uhrzeit/src/UhrPanel.java
@@ -3,46 +3,66 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.PopupMenu;
 import java.time.LocalTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class UhrPanel extends JPanel implements Runnable {
 
-    Zahl z;
+    Zahl h1=new Zahl(0), h2=new Zahl(0), m1=new Zahl(0), m2=new Zahl(0), s1=new Zahl(0), s2=new Zahl(0);
+    
     LocalTime d;
 
     public UhrPanel(LocalTime d) {
         this.d = d;
-        System.out.println("hi1");
+        this.setLayout(new GridLayout(1, 8));
+        this.setSize(1800, 302);
+        update();
+        this.add(h1);
+        this.add(h2);
+        this.add(new JLabel(":"));
+        this.add(m1);
+        this.add(m2);
+        this.add(new JLabel(":"));
+        this.add(s1);
+        this.add(s2);
+
     }
 
     @Override
     public void run() {
-        
-                 
-            
-        
-        System.out.println("hi");
-        this.setLayout(new GridLayout(1, 8));
-        this.setSize(1800, 302);
 
+        while (true) {
+            update();
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(UhrPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+    public void update() {
+        d = LocalTime.now();
         int zahl = d.getHour();
         String s = String.format("%02d", zahl);
-        this.add(new Zahl(Integer.parseInt(s.substring(0, 1))));
-        this.add(new Zahl(Integer.parseInt(s.substring(1, 2))));
+       h1.setIcon(Integer.parseInt(s.substring(0, 1)));
+       h2.setIcon(Integer.parseInt(s.substring(1, 2)));
 
         zahl = d.getMinute();
         s = String.format("%02d", zahl);
-        this.add(new Zahl(Integer.parseInt(s.substring(0, 1))));
-        this.add(new Zahl(Integer.parseInt(s.substring(1, 2))));
+        m1 = (new Zahl(Integer.parseInt(s.substring(0, 1))));
+        m2 = (new Zahl(Integer.parseInt(s.substring(1, 2))));
 
         zahl = d.getSecond();
         s = String.format("%02d", zahl);
-        this.add(new Zahl(Integer.parseInt(s.substring(0, 1))));
-        this.add(new Zahl(Integer.parseInt(s.substring(1, 2))));
-
+        s1.setIcon(Integer.parseInt(s.substring(0, 1)));
+        s2.setIcon(Integer.parseInt(s.substring(1, 2)));
         repaint();
-        
+
     }
 
 }
--- Exercise_401_Uhrzeit/src/Zahl.java
+++ Exercise_401_Uhrzeit/src/Zahl.java
@@ -7,7 +7,15 @@ public class Zahl extends JLabel {
 
     public Zahl(int i) {
         this.setSize(180, 302);
+        System.out.println("Zahl: " + i);
+        Image image = new ImageIcon("./bilder/" + i + ".png").getImage().getScaledInstance(180, 302, Image.SCALE_SMOOTH);
 
+        this.setIcon(new ImageIcon(image));
+
+        repaint();
+
+    }
+    public void setIcon(int i){
         Image image = new ImageIcon("./bilder/" + i + ".png").getImage().getScaledInstance(180, 302, Image.SCALE_SMOOTH);
 
         this.setIcon(new ImageIcon(image));
